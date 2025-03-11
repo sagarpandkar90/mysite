2ebee33e9882157ee7939d85921fc000f7db33b0
sagar <spandkar90@gmail.com>
2025-03-11 22:58:12 +0530

    Initial commit

---
+++ blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Comments
+
+class CommentForm(forms.ModelForm):
+    class Meta: 
+        model = Comments
+        exclude = ["post"]
+        labels = {
+            "user_name": "Your Name:",
+            "user_email": "Your Email:",
+            "text": "Your Comment:"
+            
+        }
